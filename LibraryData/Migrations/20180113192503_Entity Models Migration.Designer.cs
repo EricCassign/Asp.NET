179e90e275383de26bc19dc164956261cd00baf1
Eric Pro <Eric Pro@ERICPRO-PC.virtualcity.co.ke>
2018-01-13 22:26:20 +0300

    Run Initial Migaration

--- LibraryData/Migrations/20180113192503_Entity Models Migration.Designer.cs
+++ LibraryData/Migrations/20180113192503_Entity Models Migration.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace LibraryData.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20180113192503_Entity Models Migration")]
+    partial class EntityModelsMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
