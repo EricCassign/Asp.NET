3226698adece996028a02c16c1a761eb2f02d6cd
Eric Pro <Eric Pro@ERICPRO-PC.virtualcity.co.ke>
2018-01-28 11:19:25 +0300

    Added library asset service

---
+++ LibraryData/ILibraryAsset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using LibraryData.Models;
+
+namespace LibraryData
+{
+  public interface ILibraryAsset
+  {
+    IEnumerable<LibraryAsset> GetAll();
+    LibraryAsset GetById(int id);
+    void Add(LibraryAsset libraryAsset);
+    string GetAuthorOrDirector(int id);
+    string GetTitle(int id);
+    string GetISBN(int id);
+
+    LibraryBranch GetCurrentLibraryBranch(int id);
+
+  }
+}
