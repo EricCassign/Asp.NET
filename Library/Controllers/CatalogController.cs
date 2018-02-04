7326a58d2a3e699ea7ed94f2b871b920661a64e4
Eric Pro <Eric Pro@ERICPRO-PC>
2018-02-04 12:23:41 +0300

    Implemented get by Id method

--- Library/Controllers/CatalogController.cs
+++ Library/Controllers/CatalogController.cs
@@ -21,8 +21,8 @@ namespace Library.Controllers
         // GET: Catalog
         public ActionResult Index()
         {
-          var asstModels = _asset.GetAll();
-          var listingResult = asstModels.Select(
+          var assetModels = _asset.GetAll();
+          var listingResult = assetModels.Select(
             result => new LibraryAssetDto
             {
               Id = result.Id,
@@ -37,80 +37,100 @@ namespace Library.Controllers
           };
             return View(model);
         }
-        //// GET: Catalog/Details/5
-        //public ActionResult Details(int id)
-        //{
-        //    return View();
-        //}
+    // GET: Catalog/Details/5
+    public ActionResult Details(int id)
+    {
+      var asset = _asset.GetById(id) ?? throw new ArgumentNullException("_asset.GetById(id)");
+      var model = new LibraryAssetDetailsDTO
+      {
+          Id = id,
+          Title = asset.Title,
+          Year = asset.Year,
+          Cost = asset.Cost,
+          AuthorOrDirector = _asset.GetAuthorOrDirector(id),
+          ImageUrl = asset.ImageUrl,
+          ISBN = _asset.GetIsbn(id),
+          CurrentLocation = _asset.GetCurrentLibraryBranch(id).Name,
+          Status = asset.Status.Name,
+          NumberOfCopies = asset.NumberOfCopies,
+          Type = _asset.GetType(id)
 
-        //// GET: Catalog/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: Catalog/Create
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create(IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add insert logic here
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: Catalog/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: Catalog/Edit/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add update logic here
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: Catalog/Delete/5
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: Catalog/Delete/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Delete(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add delete logic here
-
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
+      };
+      //var model=new LibraryAssetDetailsApi
+      //{
+      //  LibraryAssetDetails = model
+      //};
+      return View(model);
     }
+
+    //// GET: Catalog/Create
+    //public ActionResult Create()
+    //{
+    //    return View();
+    //}
+
+    //// POST: Catalog/Create
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public ActionResult Create(IFormCollection collection)
+    //{
+    //    try
+    //    {
+    //        // TODO: Add insert logic here
+
+    //        return RedirectToAction(nameof(Index));
+    //    }
+    //    catch
+    //    {
+    //        return View();
+    //    }
+    //}
+
+    //// GET: Catalog/Edit/5
+    //public ActionResult Edit(int id)
+    //{
+    //    return View();
+    //}
+
+    //// POST: Catalog/Edit/5
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public ActionResult Edit(int id, IFormCollection collection)
+    //{
+    //    try
+    //    {
+    //        // TODO: Add update logic here
+
+    //        return RedirectToAction(nameof(Index));
+    //    }
+    //    catch
+    //    {
+    //        return View();
+    //    }
+    //}
+
+    //// GET: Catalog/Delete/5
+    //public ActionResult Delete(int id)
+    //{
+    //    return View();
+    //}
+
+    //// POST: Catalog/Delete/5
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public ActionResult Delete(int id, IFormCollection collection)
+    //{
+    //    try
+    //    {
+    //        // TODO: Add delete logic here
+
+    //        return RedirectToAction(nameof(Index));
+    //    }
+    //    catch
+    //    {
+    //        return View();
+    //    }
+    //}
+
+  }
 }
