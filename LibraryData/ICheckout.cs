d2ebe9d55d3b31a0ac70b16de5d7a3df11ea3ea1
Eric Pro <Eric Pro@ERICPRO-PC>
2018-02-04 22:55:26 +0300

    Added checkout interface

---
+++ LibraryData/ICheckout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LibraryData.Models;
+
+namespace LibraryData
+{
+  public interface ICheckout
+  {
+    IEnumerable<Checkout> GetAll();
+    Checkout GetById(int checkout);
+    void Add(Checkout checkout);
+    void CheckOutAsset(int assetId, int libraryCardId);
+    IEnumerable<CheckoutHistory> GetCheckoutHistories(int id);
+    Checkout GetLatestCheckout(int assetId);
+
+    bool IsCheckedOut(int assetId);
+    void PlaceHold(int assetId, int libraryCardId);
+    Patron GetCurrentHoldPatron(int id);
+    Patron GetCurrentCheckoutPatron(int assetId);
+    DateTime GetCurrentHoldPlacedTime(int id);
+    IEnumerable<Hold> GetCurrentHolds(int id);
+
+    void CheckInItem(int assetId);
+    void MarkLost(int assetId);
+    void MarkFound(int assetId);
+  }
+}
