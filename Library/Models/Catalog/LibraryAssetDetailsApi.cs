3888984018ec52f036ed205bf77dcdacd90477ba
Eric Pro <Eric Pro@ERICPRO-PC>
2018-02-04 12:24:19 +0300

    Added this api - to be implemented

---
+++ Library/Models/Catalog/LibraryAssetDetailsApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.Catalog
+{
+    public class LibraryAssetDetailsApi
+    {
+    public  LibraryAssetDetailsDTO LibraryAssetDetails { get; set; }
+    }
+}
